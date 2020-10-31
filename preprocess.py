29e15e2e4267c735e98cb8756a2010d647357433
Matt Deeds <mdeeds@gmail.com>
2020-10-30 19:20:37 -0700

    Simplify logic for centroid.

--- preprocess.py
+++ preprocess.py
@@ -4,8 +4,8 @@ import os
 import os.path
 import numpy as np
 from subprocess import call
-import cv2
-from PIL import Image 
+import cv2  # pip3 install opencv-python
+from PIL import Image   # pip3 install Pillow
 import random
 import math
 
@@ -30,10 +30,11 @@ def preprocess():
 def extractFrames(filename):
     src = os.path.join(filename)
     filename_no_ext = filename.split('.')[0]
-    filename_no_ext = filename_no_ext.split('\\')[-1]
+    filename_no_ext = os.path.split(filename_no_ext)[-1]
     dest = os.path.join(srcPath,'temp',filename_no_ext + '-%04d.jpg')
     #cmd = "ffmpeg -i " + src + ' -vf "scale=640:360, fps=fps=1" ' +  dest
-    cmd = "ffmpeg -i " + src + ' -vf "fps=fps=1" ' +  dest
+    ##cmd = "ffmpeg -i " + src + ' -vf "select=not(mod(n\,100))" ' +  dest
+    cmd = ["ffmpeg", "-i", src, "-vf", "scale=640:360", "-r", "1", dest]
     call(cmd)
 
 def makeBackground():
@@ -49,70 +50,42 @@ def maskFrames(background):
     files = glob.glob(os.path.join(srcPath,'temp','*.jpg'))
     for filename in files:
         image = cv2.imread(filename)
-        mask = (imageDilate(abs(image - background)) > 45).astype(np.uint8)
-        x,y = centroid(mask)
-        if(x<0):continue
-        mask = imageDilate(imageErode(toGrey(mask),size=10), size=20)
-        image_masked = image * mask
+        delta = (np.amax(abs(image - background), axis=2))
+        x,y = centroid(delta)
+        if(x<0):
+          continue
         image_cropped = image[y-149:y+150, x-149:x+150]
         filename_no_ext = filename.split('.')[0]
-        newFileName = filename_no_ext.replace("temp","frames") + ".png"
+        newFileName = filename_no_ext.replace("temp","frames") + ".jpg"
         cv2.imwrite(newFileName, image_cropped)
-        newFileName = filename_no_ext.replace("temp","frames") + "_mask.png"
-        cv2.imwrite(newFileName, image_masked)
+        # newFileName = filename_no_ext.replace("temp","frames") + "_mask.png"
+        # cv2.imwrite(newFileName, image_masked)
         #newFileName = filename_no_ext.replace("temp","frames") + "_image.png"
         #cv2.imwrite(newFileName, image)
         print("saved "+filename_no_ext)
 
-def centroid(mask):
+def centroid(delta):
     sumx=0
     sumy=0
     denominator=0
-    height,width,depth = mask.shape
-    mask=imageErode(mask,20)
-    for y,row in enumerate(mask): #1920x1080 takes about a minute
-       for x,pixel in enumerate(row):
-           if pixel.any()>0:
-               sumx = sumx + x
-               sumy = sumy + y
-               denominator + denominator + 1
-    # for x in range(width):  # 1920x1080 takes about a minute
-    #     for y in range(height):
-    #         if(mask[y,x].any()>0):
-    #            sumx = sumx + x
-    #            sumy = sumy + y
-    #            denominator = denominator + 1
-    # mask1d=mask.reshape(-1,3) # 1920x1080 takes about a minute
-    # for i,p in enumerate(mask1d):
-    #        if p.any()>0:
-    #            x=i%width
-    #            y=math.floor(i/width)
-    #            sumx = sumx + x
-    #            sumy = sumy + y
-    #            denominator + denominator + 1    
-    # mask1d=mask.reshape(-1) # 1920x1080 took 3 minutes
-    # for i,p in enumerate(mask1d):
-    #        if p.any()>0:
-    #            x=i%(width*3)
-    #            y=math.floor(i/(width*3))
-    #            sumx = sumx + x
-    #            sumy = sumy + y
-    #            denominator + denominator + 1
+    height, width = delta.shape
+    for x in range(0, width):
+       for y in range(0, height):
+         d = delta[y][x]
+         sumx += x * d
+         sumy += y * d
+         denominator += d
 
     if(denominator>0):
         x = sumx / denominator
         y = sumy / denominator
-        if x<150:x=150
-        if y<150:y=150
-        if x>(width-150):x=width-150
-        if y>(height-150):y=height-150
+        # this pair of "max" and "min" implements a "clamp".
+        x = max(150, min(width-150, x))
+        y = max(150, min(height-150, y))
     else:
         x=-1
         y=-1
     return int(x),int(y)
-    
-                
-
 
 def imageDilate(img, size=3):
     kernel = np.ones((size,size), np.uint8) 
@@ -152,9 +125,6 @@ def main():
         maskFrames(background)
         #clearTemp()
         #videoToArchive(filename)
-
-
-
 
 if __name__ == '__main__':
     main()
