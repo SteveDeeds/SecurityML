872c7bc1f3e9fd7dea7975b89df257f11dd85c51
SteveDeeds <70077400+SteveDeeds@users.noreply.github.com>
2020-11-14 18:44:18 -0800

    formatted validate.py

--- validate.py
+++ validate.py
@@ -13,12 +13,12 @@ import SMLsettings
 
 settings = SMLsettings.getSettings()
 
-global srcPath 
+global srcPath
 srcPath = os.path.join(*settings["FrameDestinationPath"])
 global dstPath
 dstPath = os.path.join(*settings["MachineSortedPath"])
-os.makedirs(dstPath,exist_ok=True)
-os.makedirs(os.path.join(dstPath,"unsure"),exist_ok=True)
+os.makedirs(dstPath, exist_ok=True)
+os.makedirs(os.path.join(dstPath, "unsure"), exist_ok=True)
 global modelPath
 modelPath = os.path.join(*settings["ModelPath"])
 
@@ -26,7 +26,8 @@ trainPath = os.path.join(*settings["TrainingPath"])
 global classes
 classes = os.listdir(trainPath)
 for c in classes:
-    os.makedirs(os.path.join(dstPath,c),exist_ok=True)
+    os.makedirs(os.path.join(dstPath, c), exist_ok=True)
+
 
 def main(nb_images=5):
     """Spot-check `nb_images` images."""
@@ -36,8 +37,8 @@ def main(nb_images=5):
     random.shuffle(images)
     print("found %d images." % len(images))
 
-    #Load the most recent model
-    max_mtime =0
+    # Load the most recent model
+    max_mtime = 0
     files = glob.glob(os.path.join(modelPath, '*.hdf5'))
     for fname in files:
         mtime = os.stat(fname).st_mtime
@@ -62,8 +63,9 @@ def main(nb_images=5):
         for i, label in enumerate(classes):
             label_predictions[label] = predictions[0][i]
 
-        sorted_lps = sorted(label_predictions.items(), key=operator.itemgetter(1), reverse=True)
-        
+        sorted_lps = sorted(label_predictions.items(),
+                            key=operator.itemgetter(1), reverse=True)
+
         for i, class_prediction in enumerate(sorted_lps):
             # Just get the top five.
             if i > 4:
@@ -71,15 +73,19 @@ def main(nb_images=5):
             print("%s: %.2f" % (class_prediction[0], class_prediction[1]))
             i += 1
         # sort the files into folders by category
-        filename = image.split('\\')[-1]
-        if(sorted_lps[0][1]>0.80):
+        filename = os.path.split(image)[1]
+        if(sorted_lps[0][1] > 0.80):
             name, extention = filename.split('.')
-            dest = os.path.join(dstPath,sorted_lps[0][0], name + '{:2d}'.format(int(sorted_lps[0][1]*100))+ '.' + extention)
+            dest = os.path.join(
+                dstPath, sorted_lps[0][0], name + '{:2d}'.format(int(sorted_lps[0][1]*100)) + '.' + extention)
             if(os.path.isfile(dest)):
                 os.remove(dest)
             os.rename(image, dest)
         else:
-            dest = os.path.join(dstPath,"unsure", filename)
+            fname, extention = filename.split(".")
+            filename = fname + "_" + \
+                sorted_lps[0][0] + "_" + sorted_lps[1][0] + "." + extention
+            dest = os.path.join(dstPath, "unsure", filename)
             if(os.path.isfile(dest)):
                 os.remove(dest)
             os.rename(image, dest)
